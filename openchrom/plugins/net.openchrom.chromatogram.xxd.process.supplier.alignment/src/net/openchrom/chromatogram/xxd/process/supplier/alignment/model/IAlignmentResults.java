640a530a023d7947b8f50ec8448cbc6a638fe439
loge <loge@10.0.1.10>
2016-08-12 11:13:15 +0200

    Start setting up Files needed for Alignment implementation

---
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.alignment/src/net/openchrom/chromatogram/xxd/process/supplier/alignment/model/IAlignmentResults.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Lorenz Gerber.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * Lorenz Gerber - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.chromatogram.xxd.process.supplier.alignment.model;
+
+import java.util.List;
+
+public interface IAlignmentResults {
+
+	List<IDataInputEntry> getDataInputEntries();
+
+	int getRetentionTimeWindow();
+
+	void setRetentionTimeWindow(int retentionTimeWindow);
+}
