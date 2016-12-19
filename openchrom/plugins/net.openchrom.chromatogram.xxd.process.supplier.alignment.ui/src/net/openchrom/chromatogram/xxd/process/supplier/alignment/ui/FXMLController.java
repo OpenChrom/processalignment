d90390532d34bf6b6757b9b978accc03fc921f25
Lorenz Gerber <lorenz.gerber@lablicate.com>
2016-12-19 17:19:06 +0100

    minimal JavaFX example

--- openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.alignment.ui/src/net/openchrom/chromatogram/xxd/process/supplier/alignment/ui/FXMLController.java
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.alignment.ui/src/net/openchrom/chromatogram/xxd/process/supplier/alignment/ui/FXMLController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 Lablicate UG (haftungsbeschränkt).
+ * Copyright (c) 2016 loge.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,29 +7,38 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * Lorenz Gerber - initial API and implementation
+ * loge - initial API and implementation
  *******************************************************************************/
-package net.openchrom.chromatogram.xxd.process.supplier.alignment.ui.parts;
+package net.openchrom.chromatogram.xxd.process.supplier.alignment.ui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.XYChart;
 
-@SuppressWarnings("rawtypes")
 public class FXMLController implements Initializable {
 
 	@FXML
 	private AreaChart areaChart;
 	private XYChart.Series series;
 
-	@SuppressWarnings("unchecked")
 	private void createChart() {
 
 		series = new XYChart.Series();
+		series.setName("Fancy stuff");
+		XYChart.Series series2 = new XYChart.Series();
+		series2.setName("Fancy stuff 2");
+		for(int i = 0; i < 100; i++) {
+			series.getData().add(new XYChart.Data(i, Math.random()));
+		}
+		for(int i = 0; i < 100; i++) {
+			series2.getData().add(new XYChart.Data(i, Math.random()));
+		}
 		areaChart.getData().addAll(series);
+		areaChart.getData().addAll(series2);
 	}
 
 	@Override
---
+++ openchrom/plugins/net.openchrom.chromatogram.xxd.process.supplier.alignment.ui/src/net/openchrom/chromatogram/xxd/process/supplier/alignment/ui/views/SamplePart.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2016 loge.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * loge - initial API and implementation
+ *******************************************************************************/
+package net.openchrom.chromatogram.xxd.process.supplier.alignment.ui.views;
+
+import java.net.URL;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+import net.openchrom.chromatogram.xxd.process.supplier.alignment.ui.FXMLController;
+
+import javafx.application.Platform;
+import javafx.embed.swt.FXCanvas;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class SamplePart {
+
+	private FXMLController controller;
+	private FXCanvas fxCanvas;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+
+		init(parent);
+	}
+
+	private void createScene() {
+
+		System.out.println("Initializing FX");
+		try {
+			final URL location = getClass().getResource("/fxml/FXML.fxml");
+			final FXMLLoader fXMLLoader = new FXMLLoader();
+			fXMLLoader.setLocation(location);
+			fXMLLoader.setBuilderFactory(new JavaFXBuilderFactory());
+			final Parent root = fXMLLoader.load(location.openStream());
+			final Scene scene = new Scene(root);
+			scene.getStylesheets().add("/styles/fxml.css");
+			fxCanvas.setScene(scene);
+			controller = fXMLLoader.getController();
+			System.out.println("Initializing FX successful");
+		} catch(final Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void init(Composite parent) {
+
+		// this will initialize the FX Toolkit
+		fxCanvas = new FXCanvas(parent, SWT.NONE);
+		Platform.setImplicitExit(false);
+		Platform.runLater(() -> createScene());
+	}
+}
